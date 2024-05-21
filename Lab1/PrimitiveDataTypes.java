f979e1216f43250c5e983cb0b25b118d611d5ba7
sheshana-123 <sheshanasure@gmail.com>
2024-05-21 13:18:57 +0530

    Lab-1

---
+++ Lab1/PrimitiveDataTypes.java
@@ -0,0 +1,34 @@
+package sample;
+
+public class PrimitiveDataTypes {
+      public static void main(String[] args) {
+    	  
+    	  char c = 'c';
+    	  System.out.println("char  "  +c);
+    	  int i = 99;
+    	  System.out.println("int  "  +i);
+    	  byte b = 4;
+    	  System.out.println("byte  "  +b);
+    	  short s = 50;
+    	  System.out.println("short  "  +s);
+    	  double d = 9.98;
+    	  System.out.println("double  "  +d);
+    	  float f = 1.246f;
+    	  System.out.println("float  "  +f);
+    	  long l = 28765;
+    	  System.out.println("long  "  +l);
+    	  boolean bool= true;
+    	  System.out.println("boolean  "  +bool);
+    	  
+
+
+
+
+
+    	  
+
+    	  
+    			  
+    	  
+      }
+}
---
+++ Lab1/SwapToAdd.java
@@ -0,0 +1,21 @@
+
+public class SwapToAdd {
+
+	public static void swap(int a,int b) {
+      int temp=0;
+      temp=a;
+      b=temp;
+	}
+       public static void add(int a,int b) {
+    	   int c=0;
+    	   c=a+b;
+    	   System.out.println(c);
+    	   
+       }
+       public static void main(String[]args) {
+    	   int a=10;
+    	   int b=20;
+    	   swap(a,b);
+    	   add(a,b);
+       }
+}
